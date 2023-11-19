75c3cdba42fe7dd44fd2464c15b352cacd010029
Niklas Trzaska <niklastrzaska@MacBook-Air-von-Niklas.local>
2023-11-19 13:58:48 +0100

    fixing file format in path

--- meta_data_test/meta_data_test.py
+++ meta_data_test/meta_data_test.py
@@ -3,7 +3,7 @@ sys.path.append('..')
 
 import meta_data
 
-basic_metadata = meta_data.get_basic_properties("testimage_01.jpg")
+basic_metadata = meta_data.get_basic_properties("testimage_01.webp")
 
 def test_basic_metadata():
     assert basic_metadata == ('WEBP', (1024, 1024), 'RGB')
