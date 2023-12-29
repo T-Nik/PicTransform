cc1df7a424a67de98c212f61c497d691303f4109
Niklas Trzaska <niklastrzaska@MacBook-Air-von-Niklas.local>
2023-12-29 20:41:04 +0100

    update meta data functionality

--- ImageMetaData_Module.py
+++ ImageMetaData_Module.py
@@ -1,6 +1,7 @@
 import logging
 from PIL import Image
 import exif
+import json 
 
 from ImageErrorHandling_Module import ImageErrorHandling
 
@@ -14,55 +15,42 @@ class ImageMetaData:
 
         # Setzt die Attribute der Klasse: Dateipfad und grundlegende Eigenschaften des Bildes
         self.relative_filepath = relative_filepath
-        self.attr_basic_properties = self.__get_basic_properties()
-        
-        # Prüft, ob das Bildformat EXIF-Informationen unterstützt und ob solche vorhanden sind
-        if self.__exif_supported_format() and self.__has_exif():
-            self.attr_has_exif = self.__has_exif()
-            self.attr_exif_attributes_of_image = self.__get_exif_attributes_of_image()
-        else:
-            # Setzt die EXIF-Attribute auf False bzw. eine leere Liste, falls nicht unterstützt oder nicht vorhanden
-            self.attr_has_exif = False
-            self.attr_exif_attributes_of_image = []
+
+
 
     # Getter-Methoden
     def get_basic_properties(self):
         # Gibt die grundlegenden Eigenschaften des Bildes zurück
-        return self.attr_basic_properties
+        return self.__get_basic_properties()
     
     def has_exif(self):
         # Gibt zurück, ob das Bild EXIF-Attribute hat
-        return self.attr_has_exif
+        return self.__has_exif()
     
-    def get_exif_attributes_of_image(self):
+    def get_exif_values(self):
         # Gibt die EXIF-Attribute des Bildes zurück
-        return self.attr_exif_attributes_of_image
+        if self.__exif_supported_format() and self.__has_exif():
+            return json.loads(json.dumps(self.__get_exif_attributes_and_values_of_image()))
+        else:
+            return []
     
-    def get_supported_exif_attributes(self):
-        # Listet alle unterstützten EXIF-Attribute auf
-        return ['_exif_ifd_pointer', '_gps_ifd_pointer', 'aperture_value', 'brightness_value', 'color_space',
- 'components_configuration', 'compression', 'datetime', 'datetime_digitized', 'datetime_original', 'exif_version',
- 'exposure_bias_value', 'exposure_mode', 'exposure_program', 'exposure_time', 'f_number', 'flash',
- 'flashpix_version', 'focal_length', 'focal_length_in_35mm_film', 'gps_altitude', 'gps_altitude_ref',
- 'gps_datestamp', 'gps_dest_bearing', 'gps_dest_bearing_ref', 'gps_horizontal_positioning_error',
- 'gps_img_direction', 'gps_img_direction_ref', 'gps_latitude', 'gps_latitude_ref', 'gps_longitude',
- 'gps_longitude_ref', 'gps_speed', 'gps_speed_ref', 'gps_timestamp', 'jpeg_interchange_format',
- 'jpeg_interchange_format_length', 'lens_make', 'lens_model', 'lens_specification', 'make', 'maker_note',
- 'metering_mode', 'model', 'orientation', 'photographic_sensitivity', 'pixel_x_dimension', 'pixel_y_dimension',
- 'resolution_unit', 'scene_capture_type', 'scene_type', 'sensing_method', 'shutter_speed_value', 'software',
- 'subject_area', 'subsec_time_digitized', 'subsec_time_original', 'white_balance', 'x_resolution',
- 'y_and_c_positioning', 'y_resolution']
+    def delete_EXIF_metadata(self):
+        self.__remove_exif_and_overwrite(self)
+        return self.get_exif_values(self)
+
+    
+
 
     # Hilfsmethoden
     def __get_basic_properties(self):
         # Protokolliert den Aufruf der Methode und versucht, die grundlegenden Eigenschaften des Bildes zu ermitteln
-        self.logger.info("method: get_basic_properties")
+        self.logger.info("method: __get_basic_properties")
         try:
             with Image.open(self.relative_filepath) as image:
-                # Öffnet das Bild und extrahiert Format, Größe und Modus
+                # Öffnet das Bild und extrahiert Format, Größe und Modus (Farbsarstellung RGB, RGBA, ...)
                 basic_properties = (image.format, image.size, image.mode)
-                self.logger.info(f"get_basic_properties: relative_filepath: {self.relative_filepath}")
-                self.logger.info(f"get_basic_properties: basic_properties: {basic_properties}")
+                self.logger.info(f"__get_basic_properties: relative_filepath: {self.relative_filepath}")
+                self.logger.info(f"__get_basic_properties: basic_properties: {basic_properties}")
                 return basic_properties
         except Exception as error:
             # Fehlerbehandlung im Falle eines Fehlers beim Öffnen des Bildes
@@ -71,12 +59,12 @@ class ImageMetaData:
     ## EXIF
     def __exif_supported_format(self):
         # Überprüft, ob das Bildformat EXIF-Informationen unterstützen kann
-        self.logger.info("method: exif_supported_format")
+        self.logger.info("method: __exif_supported_format")
         return self.get_basic_properties()[0] in ('TIFF', 'JPEG')
 
     def __has_exif(self):
         # Überprüft, ob das Bild EXIF-Informationen enthält
-        self.logger.info("method: has_exif")
+        self.logger.info("method: __has_exif")
         if not self.__exif_supported_format():
             return False
         try:
@@ -87,18 +75,43 @@ class ImageMetaData:
             # Fehlerbehandlung, falls ein Fehler beim Zugriff auf die EXIF-Informationen auftritt
             ImageErrorHandling.handle_error(self.logger, error, self.relative_filepath)
 
-    def __get_exif_attributes_of_image(self):
-        # Liest und gibt die EXIF-Attribute des Bildes zurück
-        self.logger.info("method: get_exif_properties")
+    def __get_exif_attributes_and_values_of_image(self):
+        # Liest und gibt die EXIF-Attribute und Werte des Bildes zurück
+        self.logger.info("method: __get_exif_attributes_and_values_of_image")
         if not self.__exif_supported_format():
             return "Format not supported"
         try:
             with open(self.relative_filepath, 'rb') as image_file:
                 image = exif.Image(image_file)
-                exif_properties = image.list_all()
-                self.logger.info(f"get_exif_properties: relative_filepath: {self.relative_filepath}")
-                self.logger.info(f"get_exif_properties: basic_properties: {exif_properties}")
+                exif_properties = image.get_all()
+                self.logger.info(f"__get_exif_attributes_and_values_of_image: relative_filepath: {self.relative_filepath}")
+                self.logger.info(f"__get_exif_attributes_and_values_of_image: basic_properties: {exif_properties}")
                 return exif_properties
         except Exception as error:
             # Fehlerbehandlung, falls ein Fehler beim Lesen der EXIF-Attribute auftritt
             ImageErrorHandling.handle_error(self.logger, error, self.relative_filepath)
+
+
+    def __remove_exif_and_overwrite(self):
+        # Entfernt EXIF-Metadaten aus dem Bild und überschreibt das Originalbild.
+   
+        self.logger.info("method: __remove_exif_and_overwrite")
+        if self.has_exif():
+            try:
+                # Bild ohne EXIF-Daten öffnen
+                with Image.open(self.relative_filepath) as img:
+                    data = img.getdata()
+
+                    # Neues Bild ohne EXIF-Daten erstellen
+                    new_img = Image.new(img.mode, img.size)
+                    new_img.putdata(data)
+
+                    # Bild am gleichen Pfad überschreiben
+                    new_img.save(self.relative_filepath, quality=95, optimize=True)
+                    
+                    self.logger.info("__remove_exif_and_overwrite: EXIF-Daten entfernt und Bild erfolgreich überschrieben.")
+
+            except Exception as error:
+                # Fehlerbehandlung
+                ImageErrorHandling.handle_error(self.logger, error, self.relative_filepath)
+                self.logger.error(f"Fehler beim Entfernen der EXIF-Daten: {error}")
--- meta_data_test/meta_data_test.py
+++ meta_data_test/meta_data_test.py
@@ -1,20 +1,27 @@
 import sys
-sys.path.append('..')
+import os
 from ImageMetaData_Module import ImageMetaData
 
 
+
 def test_basic_metadata_webp():
-    image = ImageMetaData("testimage_01.webp")
+    print("Aktuelles Arbeitsverzeichnis:", os.getcwd())
+
+    image = ImageMetaData("meta_data_test/testimage_01.webp")
     basic_metadata = image.get_basic_properties()
     assert basic_metadata == ('WEBP', (1024, 1024), 'RGB')
     assert image.has_exif() == False
-    assert image.get_exif_attributes_of_image() == []
-
+"""     assert image.get_exif_attributes_of_image() == []
+ """
 def test_basic_metadata_jpg():
-    image = ImageMetaData("Canon_40D.jpg")
+    print("Aktuelles Arbeitsverzeichnis:", os.getcwd())
+
+    image = ImageMetaData("meta_data_test/Canon_40D.jpg")
     basic_metadata = image.get_basic_properties()
     assert basic_metadata == ('JPEG', (100, 77), 'RGB')
     assert image.has_exif() == True
 
-    exif_metadata = image.get_exif_attributes_of_image()
-    assert exif_metadata == ['orientation', 'x_resolution', 'y_resolution', 'resolution_unit', 'software', 'datetime', '_exif_ifd_pointer', 'compression', 'jpeg_interchange_format', 'jpeg_interchange_format_length', 'color_space', 'pixel_x_dimension', 'pixel_y_dimension']
+    exif_metadata = image.get_exif_values()
+    assert exif_metadata["orientation"] == 1
+    assert exif_metadata["x_resolution"] == 300.0
+    assert exif_metadata["y_resolution"] == 300.0
