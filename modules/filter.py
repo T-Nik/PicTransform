d9fac8ec5acb50c80aa8f72052fee1bc4c19bb82
Jennifer Beckheuer <jennifer.beckheuer@hotmail.com>
2024-01-01 19:16:38 +0100

    Bug gefixed, Filter_Presets-Klasse entfernt

--- modules/filter.py
+++ modules/filter.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageEnhance
 
+
 class Filter:
 
     __slots__ = ["name",
@@ -29,22 +30,11 @@ class Filter:
         self.set_current_preset()
         image = Image.open(image_path)
 
-        def adjust_contrast(image):
-            im_contrast = ImageEnhance.Contrast(image)
-            return im_contrast.enhance(self.contrast)
-
-        def adjust_brightness(image):
-            im = ImageEnhance.Brightness(image)
-            return im.enhance(self.brightness)
-
-        def adjust_saturation(image):
-            im = ImageEnhance.Color(image)
-            return im.enhance(self.saturation)
-
         def apply_threshold(image):
 
             if (thresh := self.color_thresholds[3]):
-                return image.convert("L").point((lambda x: 255 if x > thresh else 0), mode="1")
+                i = image.convert("L").point((lambda x: 255 if x > thresh else 0), mode="1")
+                return i.convert("RGB")
 
             rgb_channels = []
 
@@ -63,48 +53,35 @@ class Filter:
             return Image.merge("RGB", (r_channel, _, b_channel))
 
         if self.contrast != float(1.0):
-            image = adjust_contrast(image)
-            #image.show()
+            image = ImageEnhance.Contrast(image).enhance(self.contrast)
 
         if self.brightness != float(1.0):
-            image = adjust_brightness(image)
-            #image.show()
+            image = ImageEnhance.Brightness(image).enhance(self.brightness)
 
         if self.saturation != float(1.0):
-            image = adjust_saturation(image)
-            #image.show()
+            image = ImageEnhance.Color(image).enhance(self.saturation)
 
         if self.color_temperature_change != 0:
             image = adjust_color_temperature(image)
-            #image.show()
 
         if any(self.color_thresholds) is True:
             image = apply_threshold(image)
-            #image.show()
 
-        if preview:
-            image.show()
-        else:
-            image.save(image_path)
+        image.show() if preview else image.save(image_path)
 
     def set_current_preset(self):
-        Filter_Presets.current_filter = self
+        filter.current_filter = self
 
     def __str__(self):
         return str(self.name)
 
-
-
-
-class Filter_Presets:
-
-    filter_dict = {
-        "Original": Filter("Original"),
-        "Grau": Filter("Grau", saturation=0.0),
-        "Schwarzweiß": Filter("Schwarzweiß", brightness=1.2, color_thresholds=(0, 0, 0, 175)),
-        "Warm": Filter("Warm", color_temperature_change=+2),
-        "Kalt": Filter("Kalt", color_temperature_change=-2),
-        "Poster": Filter("Poster", brightness=1.0, contrast=1.2, color_thresholds=(170, 170, 170, 0))
+filter_dict = {
+    "Original": Filter("Original"),
+    "Grau": Filter("Grau", saturation=0.0),
+    "Schwarzweiß": Filter("Schwarzweiß", brightness=1.2, color_thresholds=(0, 0, 0, 175)),
+    "Warm": Filter("Warm", color_temperature_change=+2),
+    "Kalt": Filter("Kalt", color_temperature_change=-2),
+    "Poster": Filter("Poster", brightness=1.0, contrast=1.2, color_thresholds=(170, 170, 170, 0))
         }
 
-    current_filter = filter_dict["Original"]
+current_filter = filter_dict["Original"]
