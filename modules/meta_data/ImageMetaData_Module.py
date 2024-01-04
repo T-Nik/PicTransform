b5aee6f72ae25c24fcf05d4b02919fdf8454abcc
Niklas Trzaska <niklastrzaska@MacBook-Air-von-Niklas.local>
2024-01-04 11:53:03 +0100

    error handling und comments

--- modules/meta_data/ImageMetaData_Module.py
+++ modules/meta_data/ImageMetaData_Module.py
@@ -1,68 +1,69 @@
 import logging
 from PIL import Image
 import exif
-import json 
-
+import json
 from modules.meta_data.ImageErrorHandling_Module import ImageErrorHandling
 
-#logging.basicConfig(filename='pictransform.log', encoding='utf-8', level=logging.DEBUG)
+# Konfiguration des Logging-Systems für bessere Fehlernachverfolgung
 logging.basicConfig(format='%(asctime)s %(message)s')
 
 class ImageMetaData:
     def __init__(self, relative_filepath):
-        # Initialisiert den Logger für diese Klasse
-        self.logger = logging.getLogger(__name__)
+        # Initialisiert die Klasse. Jede Instanz der Klasse arbeitet mit einem spezifischen Bild, dessen Pfad hier übergeben wird.
+        self.logger = logging.getLogger(___name__)  # Erstellt einen Logger speziell für diese Klasse.
+        self.relative_filepath = relative_filepath  # Speichert den relativen Pfad zur Bilddatei.
 
-        # Setzt die Attribute der Klasse: Dateipfad und grundlegende Eigenschaften des Bildes
-        self.relative_filepath = relative_filepath
+    # Öffentliche Methoden (Teil der Schnittstelle zur Außenwelt):
+    # Diese Methoden definieren, wie andere Teile des Programms oder andere Programme mit dieser Klasse interagieren können.
+    # Sie kapseln die interne Logik der Klasse und bieten eine klare und verständliche API.
 
-
-    # Getter-Methoden
     def get_basic_properties(self):
-        # Gibt die grundlegenden Eigenschaften des Bildes zurück
+        # Bietet eine öffentliche Methode, um grundlegende Eigenschaften des Bildes abzurufen.
         return self.__get_basic_properties()
     
     def has_exif(self):
-        # Gibt zurück, ob das Bild EXIF-Attribute hat
+        # Bietet eine öffentliche Methode, um zu überprüfen, ob das Bild EXIF-Daten enthält.
         return self.__has_exif()
-    
+
     def get_exif_values(self):
-        # Gibt die EXIF-Attribute des Bildes zurück
+        # Bietet eine öffentliche Methode, um EXIF-Informationen des Bildes zu extrahieren.
         if self.__exif_supported_format() and self.__has_exif():
-            return json.loads(json.dumps(self.__get_exif_attributes_and_values_of_image()))
+            try:
+                return json.loads(json.dumps(self.__get_exif_attributes_and_values_of_image()))
+            except (TypeError, ValueError):
+                return []
         else:
             return []
-    
+
     def delete_EXIF_metadata(self):
+        # Bietet eine öffentliche Methode, um EXIF-Daten aus dem Bild zu entfernen.
         self.__remove_exif_and_overwrite()
         return self.get_exif_values()
 
-    
+    # Private Methoden (Interne Implementierungsdetails):
+    # Diese Methoden sind nur innerhalb der Klasse sichtbar und werden verwendet, um die Funktionalität zu implementieren,
+    # die von den öffentlichen Methoden bereitgestellt wird. Durch die Trennung dieser Details wird die Komplexität für
+    # den Benutzer der Klasse reduziert und die interne Logik geschützt.
 
-
-    # Hilfsmethoden
     def __get_basic_properties(self):
-        # Protokolliert den Aufruf der Methode und versucht, die grundlegenden Eigenschaften des Bildes zu ermitteln
+        # Interne Methode zur Ermittlung grundlegender Bildeigenschaften.
         self.logger.info("method: __get_basic_properties")
         try:
             with Image.open(self.relative_filepath) as image:
-                # Öffnet das Bild und extrahiert Format, Größe und Modus (Farbsarstellung RGB, RGBA, ...)
                 basic_properties = (image.format, image.size, image.mode)
                 self.logger.info(f"__get_basic_properties: relative_filepath: {self.relative_filepath}")
                 self.logger.info(f"__get_basic_properties: basic_properties: {basic_properties}")
                 return basic_properties
         except Exception as error:
-            # Fehlerbehandlung im Falle eines Fehlers beim Öffnen des Bildes
             ImageErrorHandling.handle_error(self.logger, error, self.relative_filepath)
 
-    ## EXIF
     def __exif_supported_format(self):
-        # Überprüft, ob das Bildformat EXIF-Informationen unterstützen kann
+        # Interne Überprüfung, ob das Bildformat EXIF-Informationen unterstützt.
         self.logger.info("method: __exif_supported_format")
         return self.get_basic_properties()[0] in ('TIFF', 'JPEG')
 
     def __has_exif(self):
-        # Überprüft, ob das Bild EXIF-Informationen enthält
+        # Interne Methode zur Überprüfung der EXIF-Verfügbarkeit im Bild.
         self.logger.info("method: __has_exif")
         if not self.__exif_supported_format():
             return False
@@ -71,14 +72,13 @@ class ImageMetaData:
                 image = exif.Image(image_file)
                 return image.has_exif
         except Exception as error:
-            # Fehlerbehandlung, falls ein Fehler beim Zugriff auf die EXIF-Informationen auftritt
             ImageErrorHandling.handle_error(self.logger, error, self.relative_filepath)
 
     def __get_exif_attributes_and_values_of_image(self):
-        # Liest und gibt die EXIF-Attribute und Werte des Bildes zurück
+        # Interne Methode zum Abrufen von EXIF-Attributen und deren Werten.
         self.logger.info("method: __get_exif_attributes_and_values_of_image")
         if not self.__exif_supported_format():
-            return "Format not supported"
+            return []
         try:
             with open(self.relative_filepath, 'rb') as image_file:
                 image = exif.Image(image_file)
@@ -87,30 +87,19 @@ class ImageMetaData:
                 self.logger.info(f"__get_exif_attributes_and_values_of_image: basic_properties: {exif_properties}")
                 return exif_properties
         except Exception as error:
-            # Fehlerbehandlung, falls ein Fehler beim Lesen der EXIF-Attribute auftritt
             ImageErrorHandling.handle_error(self.logger, error, self.relative_filepath)
 
-
     def __remove_exif_and_overwrite(self):
-        # Entfernt EXIF-Metadaten aus dem Bild und überschreibt das Originalbild.
-   
+        # Interne Methode zum Entfernen von EXIF-Daten und Überschreiben des Bildes.
         self.logger.info("method: __remove_exif_and_overwrite")
         if self.has_exif():
             try:
-                # Bild ohne EXIF-Daten öffnen
                 with Image.open(self.relative_filepath) as img:
                     data = img.getdata()
-
-                    # Neues Bild ohne EXIF-Daten erstellen
                     new_img = Image.new(img.mode, img.size)
                     new_img.putdata(data)
-
-                    # Bild am gleichen Pfad überschreiben
                     new_img.save(self.relative_filepath, quality=95, optimize=True)
-                    
                     self.logger.info("__remove_exif_and_overwrite: EXIF-Daten entfernt und Bild erfolgreich überschrieben.")
-
             except Exception as error:
-                # Fehlerbehandlung
                 ImageErrorHandling.handle_error(self.logger, error, self.relative_filepath)
                 self.logger.error(f"Fehler beim Entfernen der EXIF-Daten: {error}")
