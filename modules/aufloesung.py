5c27b8c604649c359739b66976ec241a3333fd4a
xjohanna <johanna.1@gmx.de>
2024-01-06 17:17:39 +0100

    add function resize image

--- modules/aufloesung.py
+++ modules/aufloesung.py
@@ -5,8 +5,32 @@ Beschreibung der Funktion:
 Mit dieser Funktion kann die Auflösung eines importierten Bildes angepasst werden.
 
 Vergleiche folgende Quellen anhand derer der Code implementiert wurde:
+    Modul Image: https://pillow.readthedocs.io/en/stable/reference/Image.html
     
 '''
-
-def aufloesung (image_path, aufloesung):
     
+#Import des Image-Moduls der Bibliothek Pillow
+from PIL import Image
+
+#Import des ImageFilter-Moduls der Bibliothek Pillow
+from PIL import ImageFilter
+
+# Der Funktion werden ein Dateipfad zum Bild sowie die gewünschte neue Auflösung (bestehtend aus Höhe und Breite) übergeben.
+def aufloesung (image_path, breite, hoehe, preview=True):
+    try:
+        im = Image.open(image_path)
+        print("Test")
+        resized_img = im.resize([hoehe, breite])
+        print("Test2")
+        #Das manipulierte Bild wird angezeigt.
+        if preview:
+            resized_img.show()
+            return "Aufloesung preview getriggered"
+        else:
+            resized_img.save(image_path)
+            return image_path + " Auflösung geändert auf Breite:" + str(breite) + " und Höhe: " + str(hoehe) + "."
+
+    except ValueError:
+            print("Es ist ein Fehler aufgetreten. Die Werte müssen über 0 liegen.")
+
+#aufloesung(r"G:\Downloads\test.jpg",600,9000, True)
