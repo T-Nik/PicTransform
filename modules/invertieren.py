335816096a5193efac0cd6557b1736866bf80040
Niklas Trzaska <niklastrzaska@MacBook-Air-von-Niklas.local>
2024-01-28 20:49:03 +0100

    Parameter-Check

--- modules/invertieren.py
+++ modules/invertieren.py
@@ -7,27 +7,38 @@ import numpy as np
 # - output_path: Der Dateipfad für die Speicherung des invertierten Bildes.
 # - preview: Gibt an, ob eine Vorschau des invertierten Bildes angezeigt werden soll (Standardwert ist False).
 def invert_image(image_path, output_path, preview=False):
-    # Öffnen Sie das Bild mit Pillow.
-    img = Image.open(image_path)
+    try:
+        # Parameter-Validierung
+        if not isinstance(image_path, str) or not isinstance(output_path, str) or not isinstance(preview, bool):
+            raise TypeError("Ungültige Parameter. 'image_path' und 'output_path' müssen Zeichenketten (Strings) sein, 'preview' muss ein boolescher Wert sein.")
 
-    # Konvertieren Sie das Bild in ein Numpy-Array.
-    image_array = np.array(img)
+        # Öffnen Sie das Bild mit Pillow.
+        img = Image.open(image_path)
 
-    # Invertiert die Farben im Bild durch Subtraktion von jedem Pixelwert von 255.
-    image_array = 255 - image_array
+        # Konvertieren Sie das Bild in ein Numpy-Array.
+        image_array = np.array(img)
 
-    # Erstellen Sie ein neues Bild mit den bearbeiteten Pixelwerten.
-    inverted_img = Image.fromarray(image_array)
+        # Invertiert die Farben im Bild durch Subtraktion von jedem Pixelwert von 255.
+        image_array = 255 - image_array
 
-    # Es wird entweder eine Vorschau des bearbeiteten Bildes angezeigt und eine entsprechende Meldung zurückgegeben, 
-    # oder das bearbeitete Bild wird gespeichert.
-    if preview:
-        print("Invertierung ausgelöst")
-        inverted_img.show()
-    else:
-        print("Invertierung ausgelöst")
-        inverted_img.save(output_path)
+        # Erstellen Sie ein neues Bild mit den bearbeiteten Pixelwerten.
+        inverted_img = Image.fromarray(image_array)
 
-    # Gibt den Dateipfad des invertierten Bildes zurück.
-    return output_path
+        # Es wird entweder eine Vorschau des bearbeiteten Bildes angezeigt und eine entsprechende Meldung zurückgegeben, 
+        # oder das bearbeitete Bild wird gespeichert.
+        if preview:
+            print("Invertierung Preview ausgelöst")
+            inverted_img.show()
+        else:
+            print("Invertierung apply ausgelöst")
+            inverted_img.save(output_path)
 
+        # Gibt den Dateipfad des invertierten Bildes zurück.
+        return output_path
+
+    except Exception as e:
+        # Falls ein Fehler auftritt, gibt eine Fehlermeldung aus.
+        print(f"Fehler bei der Invertierung des Bildes: {e}")
+
+# Beispiel für die Verwendung der Funktion
+# invert_image('path/to/input_image.jpg', 'path/to/output_image.jpg', preview=True)
