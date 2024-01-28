098f652627b7d7ef5db0365460eb291c445bf336
Niklas Trzaska <niklastrzaska@MacBook-Air-von-Niklas.local>
2024-01-28 20:18:30 +0100

    Kommentierung und Test Code entfernt

--- modules/invertieren.py
+++ modules/invertieren.py
@@ -1,36 +1,34 @@
-
-''' https://de.wikipedia.org/wiki/Invertieren_(Bildbearbeitung)#:~:text=In%20der%20Bildbearbeitung%20wird%20mit,aus%20schwarz%20wei%C3%9F%20und%20umgekehrt.'''
-
 from PIL import Image
 import numpy as np
 
-
+# Die Funktion ermöglicht die Invertierung von Farben in einem Bild.
+# Parameter:
+# - image_path: Der Dateipfad zum Bild, das invertiert werden soll.
+# - output_path: Der Dateipfad für die Speicherung des invertierten Bildes.
+# - preview: Gibt an, ob eine Vorschau des invertierten Bildes angezeigt werden soll (Standardwert ist False).
 def invert_image(image_path, output_path, preview=False):
-    # Öffnen Sie das Bild mit Pillow
+    # Öffnen Sie das Bild mit Pillow.
     img = Image.open(image_path)
 
-    # Konvertieren Sie das Bild in ein Numpy-Array
+    # Konvertieren Sie das Bild in ein Numpy-Array.
     image_array = np.array(img)
 
+    # Invertiert die Farben im Bild durch Subtraktion von jedem Pixelwert von 255.
     image_array = 255 - image_array
 
-    # Erstellen Sie ein neues Bild mit den bearbeiteten Pixelwerten
+    # Erstellen Sie ein neues Bild mit den bearbeiteten Pixelwerten.
     inverted_img = Image.fromarray(image_array)
 
-    # Bild anzeigen lassen oder speichern
+    # Entscheidet, ob eine Vorschau des invertierten Bildes angezeigt oder das Bild gespeichert werden soll.
     if preview:
+        # Wenn Vorschau gewünscht ist, gibt eine Meldung aus und zeigt die Vorschau an.
         print("Invertierung ausgelöst")
         inverted_img.show()
     else:
-        # Speichern des Bildes
+        # Wenn Vorschau nicht gewünscht ist, gibt eine Meldung aus und speichert das invertierte Bild.
         print("Invertierung ausgelöst")
-        inverted_img.save(image_path)
+        inverted_img.save(output_path)
 
-    return image_path + ""
+    # Gibt den Dateipfad des invertierten Bildes zurück.
+    return output_path
 
-    # Speichern Sie das bearbeitete Bild
-    invert_img.save(output_path)
-input_image_path = 'C:/Users/gille/Pictures/animal.jpeg'
-output_image_path = 'C:/Users/gille/inverted_image.jpeg'
-
-invert_image(input_image_path, output_image_path, preview=True)
