c9181206d3a36ec9bf9bd081cad32fd435674190
Niklas Trzaska <niklastrzaska@MacBook-Air-von-Niklas.local>
2024-01-28 21:00:47 +0100

    Parameter-Check

--- modules/aufloesung.py
+++ modules/aufloesung.py
@@ -20,6 +20,16 @@ from PIL import Image
 # - preview: Gibt an, ob eine Vorschau des veränderten Bildes angezeigt werden soll (Standardwert ist True).
 def aufloesung(image_path, breite, hoehe, preview=True):
     try:
+        # Parameter-Validierung
+        if not isinstance(image_path, str):
+            raise TypeError("Ungültiger Parameter. 'image_path' muss ein String sein.")
+        if not isinstance(breite, int) or breite <= 0:
+            raise ValueError("Ungültiger Parameter. 'breite' muss eine positive ganze Zahl sein.")
+        if not isinstance(hoehe, int) or hoehe <= 0:
+            raise ValueError("Ungültiger Parameter. 'hoehe' muss eine positive ganze Zahl sein.")
+        if not isinstance(preview, bool):
+            raise TypeError("Ungültiger Parameter. 'preview' muss ein Boolean-Wert sein.")
+
         # Öffnet das Bild mit Pillow und ändert seine Auflösung.
         im = Image.open(image_path)
         resized_img = im.resize([hoehe, breite])
@@ -33,6 +43,6 @@ def aufloesung(image_path, breite, hoehe, preview=True):
             resized_img.save(image_path)
             return image_path + " Auflösung geändert auf Breite:" + str(breite) + " und Höhe: " + str(hoehe) + "."
 
-    except ValueError:
+    except ValueError as ve:
         # Gibt eine Fehlermeldung aus, wenn ungültige Werte für Breite oder Höhe angegeben werden.
-        print("Es ist ein Fehler aufgetreten. Die Werte müssen über 0 liegen.")
+        print(f"Es ist ein Fehler aufgetreten: {ve}")
