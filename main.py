8b741762271844d06fb430d1f977837dcf956f6d
xjohanna <johanna.1@gmx.de>
2024-01-31 20:56:09 +0100

    change value auflösung -99 - +99

--- main.py
+++ main.py
@@ -275,7 +275,7 @@ class PicTransform(App):
         self.root.ids.aktions_leiste.add_widget(self.degree_label)
 
         # Slider hinzufügen
-        self.blur_slider = Slider(min=-100, max=100, value=0)
+        self.blur_slider = Slider(min=-99, max=99, value=0)
         self.blur_slider.bind(value=lambda instance, value: setattr(self.degree_label, 'text', f'{int(value)}%'))
         self.root.ids.aktions_leiste.add_widget(self.blur_slider)
 
